db8a4bbdeafb74452643bc09f969966051607cdf
Goktug Yilmaz <goktugyil@gmail.com>
2015-11-22 21:50:39 -0800

    -Made withdivision public too

--- EZLoadingActivity.swift
+++ EZLoadingActivity.swift
@@ -25,7 +25,7 @@ public struct EZLoadingActivity {
         public static var FailText = "Failure"
         public static var SuccessColor = UIColor(red: 68/255, green: 118/255, blue: 4/255, alpha: 1.0)
         public static var FailColor = UIColor(red: 255/255, green: 75/255, blue: 56/255, alpha: 1.0)
-        static var WidthDivision: CGFloat {
+        public static var WidthDivision: CGFloat {
             get {
                 if UIDevice.currentDevice().userInterfaceIdiom == UIUserInterfaceIdiom.Pad {
                     return  3.5
