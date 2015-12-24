b3728f3be7071f8a29c3767f29d7a8c99d8429bc
Ben <b.cylin@gmail.com>
2015-12-24 20:41:44 +0800

    Add a button in the example project to show loading activity

--- EZLoadingActivity/ViewController.swift
+++ EZLoadingActivity/ViewController.swift
@@ -10,22 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private lazy var button: UIButton = {
+        let _button = UIButton(type: .System)
+        _button.setTitle("Show EZLoadingActivity", forState: .Normal)
+        _button.addTarget(self, action: Selector("showLoadingActivity:"), forControlEvents: .TouchUpInside)
+        return _button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
+        view.addSubview(button)
+
+        if #available(iOS 9.0, *) {
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.centerXAnchor.constraintEqualToAnchor(view.centerXAnchor).active = true
+            button.centerYAnchor.constraintEqualToAnchor(view.centerYAnchor).active = true
+        } else {
+            button.sizeToFit()
+            button.center = CGPoint(x: view.bounds.midX, y: view.bounds.midY)
+            button.autoresizingMask = [.FlexibleTopMargin, .FlexibleLeftMargin, .FlexibleBottomMargin, .FlexibleRightMargin]
+        }
     }
 
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
+    @IBAction func showLoadingActivity(sender: UIButton) {
         EZLoadingActivity.showWithDelay("Testing..", disableUI: false, seconds: 3)
-
     }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
-
